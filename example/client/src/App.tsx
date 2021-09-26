aca12f0be6ef6498fc901172129f74a47c7ea6b3
roytang <me@roytang.me>
2021-09-26 23:34:50 +0800

    update example client

--- example/client/src/App.tsx
+++ example/client/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { RedisGrpcPromiseClient } from 'redis-web/gen-js/redis_grpc_grpc_web_pb';
-import { KeysRequest } from 'redis-web/gen-js/redis_grpc_pb';
+import { KeysRequest, SetRequest } from 'redis-web/gen-js/redis_grpc_pb';
 
 function App() {
 
@@ -13,6 +13,15 @@ function App() {
     client.keys(request).then(response => {
       console.log(response.getResultList())
     });
+
+    const perform_set = () => {
+      let set_cmd = new SetRequest();
+      set_cmd.setKey(Date.now().toString())
+      set_cmd.setValue(123 as any);
+      client.set(set_cmd).then(resp => console.log(resp.getResult())).catch(console.error)
+    }
+
+    perform_set()
   }, [])
 
   return (
